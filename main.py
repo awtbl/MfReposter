dbfd80c5e4555594b7b5d0559d62311aac876711
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-11 16:24:04 +0500

    Make main function asynchronous

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import asyncio
-
+import json
 import constants
 
 from pyrogram import Client
 from configurator import PyrogramConfig, load_config
+from models import Channel
 
 
-def client_builder(config: PyrogramConfig) -> Client:
-    """W
+def load_channel() -> Channel:
+    try:
+        file = open("old_channel_data.dat")
+    except FileNotFoundError:
+        pass
+
+
+async def client_builder(config: PyrogramConfig) -> Client:
+    """
     Returns an initialized client
     :param PyrogramConfig config: A configuration-object
     :rtype: Client
@@ -20,25 +28,23 @@ def client_builder(config: PyrogramConfig) -> Client:
         api_id=config.api_id,
     )
 
-    client.start()
+    await client.start()
 
     return client
 
 
-async def task(client, config):
-    ch = await client.get_chat(config.original_channel_id)
-
-    print(ch.id)
-    print(ch.bio)
-
-def main():
+async def main():
     """Heart of project"""
     config = load_config(constants.CONFIG_FILENAME)
+    print(config.channels.original_channel_id)
+    client = await client_builder(config.pyrogram)
+    ch = await client.get_chat(chat_id=-1001469529464)
 
-    client = client_builder(config.pyrogram)
-
-    asyncio.run(task(client, config.channels))
+    print(ch.id)
+    print(ch.description)
+    print(ch.username)
+    print(ch.title)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
