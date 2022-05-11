36e372668afa995c3b616b8031f1b11cf166f549
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-11 16:12:50 +0500

    Added loader.py

---
+++ loader.py
@@ -0,0 +1,19 @@
+from typing import Union
+from pyrogram import Client
+from models import Channel
+
+
+async def load_channel(pyrogram_client: Client, channel_id: Union[str, int]) -> Channel:
+    """
+    Fetches needed meta-information about channel
+    :param pyrogram_client: A pyrogram client
+    :param channel_id: A channel's ID
+    :return:
+    """
+    ch = await pyrogram_client.get_chat(chat_id=channel_id)
+
+    return Channel(
+        ch.photo.big_file_id,
+        ch.title,
+        ch.description
+    )
