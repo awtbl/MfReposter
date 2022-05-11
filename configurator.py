c21e0f93a324b15e49eaf1529e4a1b22d0c148c0
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-11 12:53:22 +0500

    Added channel's username providing ability

--- configurator.py
+++ configurator.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 from configparser import ConfigParser
+from typing import Union
 
 
 @dataclass
 class ChannelsConfig:
     """Configuration-class for Maf's channels"""
-    original_channel_id: int
-    duplicate_channel_id: int
+    original_channel_id: Union[str, int]
+    duplicate_channel_id: Union[str, int]
 
 
 @dataclass
@@ -21,6 +22,22 @@ class Configuration:
     """Main Config-class"""
     pyrogram: PyrogramConfig
     channels: ChannelsConfig
+
+
+def parse_telegram_id(config: ConfigParser, section: str, option: str) -> Union[str, int]:
+    """
+    Parses telegram-id
+    :param ConfigParser config: A ConfigParser-object (prepared)
+    :param Section section: A Section
+    :param option: An option which contains telegram-id
+    """
+
+    try:
+        # Firstly, try to get ID
+        return config.getint(section, option)
+    except ValueError:
+        # If we can't parse ID as int, it is username
+        return config.get(section, option)
 
 
 def load_config(filename: str) -> Configuration:
@@ -38,8 +55,8 @@ def load_config(filename: str) -> Configuration:
     )
 
     channels_config = ChannelsConfig(
-        config.getint("channels", "original"),
-        config.getint("channels", "duplicate"),
+        parse_telegram_id(config, "channels", "original"),
+        parse_telegram_id(config, "channels", "duplicate"),
     )
 
     return Configuration(
