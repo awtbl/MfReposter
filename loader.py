f20e30eaf03cd6daaa35ec150ec461c0dcaef753
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-14 16:55:09 +0500

    Reversed result, load_channel deleted

--- loader.py
+++ loader.py
@@ -1,24 +1,6 @@
 from typing import Union, List
 from pyrogram import Client
 from pyrogram.types import Message
-from models import Channel
-
-
-async def load_channel(pyrogram_client: Client, channel_id: Union[str, int]) -> Channel:
-    """
-    Fetches needed meta-information about channel
-    :param pyrogram_client: A pyrogram client
-    :param channel_id: A channel's ID
-    :return: Channel
-    """
-
-    ch = await pyrogram_client.get_chat(chat_id=channel_id)
-
-    return Channel(
-        ch.photo.big_file_id if ch.photo else None,
-        ch.title,
-        ch.description
-    )
 
 
 async def load_new_messages(pyrogram_client: Client, channel_id: Union[str, int], lst_msg: int = 1) -> List[Message]:
@@ -36,4 +18,4 @@ async def load_new_messages(pyrogram_client: Client, channel_id: Union[str, int]
             break
         result.append(message)
 
-    return result
+    return result[::-1]
