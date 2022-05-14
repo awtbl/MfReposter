a6b07ea215ef8d9d5de06efefe3c915bb667f359
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-14 13:21:28 +0500

    Fixed problem with getting Channel's profile photo ID

--- loader.py
+++ loader.py
@@ -8,12 +8,13 @@ async def load_channel(pyrogram_client: Client, channel_id: Union[str, int]) -> 
     Fetches needed meta-information about channel
     :param pyrogram_client: A pyrogram client
     :param channel_id: A channel's ID
-    :return:
+    :return: Channel
     """
+
     ch = await pyrogram_client.get_chat(chat_id=channel_id)
 
     return Channel(
-        ch.photo.big_file_id,
+        ch.photo.big_file_id if ch.photo else None,
         ch.title,
         ch.description
     )
