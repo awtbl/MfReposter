e5f1277db83377aa1bb5b97317bf15fd3d6c56bd
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-14 13:45:26 +0500

    Added load_new_messages function

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
-from typing import Union
+from typing import Union, List
 from pyrogram import Client
+from pyrogram.types import Message
 from models import Channel
 
 
@@ -18,3 +19,21 @@ async def load_channel(pyrogram_client: Client, channel_id: Union[str, int]) -> 
         ch.title,
         ch.description
     )
+
+
+async def load_new_messages(pyrogram_client: Client, channel_id: Union[str, int], lst_msg: int = 1) -> List[Message]:
+    """
+    Loads a list with messages from lst_msg to last message in chat!
+    :param pyrogram_client: A pyrogram client
+    :param channel_id: Channel's ID
+    :param lst_msg: Last READIED message ID!
+    :return: List with unread messages
+    """
+    result = []
+
+    async for message in pyrogram_client.get_chat_history(chat_id=channel_id):
+        if message.id <= lst_msg:
+            break
+        result.append(message)
+
+    return result
