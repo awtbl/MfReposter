46c07b92456e4bbdcaffe3cc9cef4fe46f87d0c0
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-14 21:06:24 +0500

    Added updating of channel's title

--- jobs.py
+++ jobs.py
@@ -17,11 +17,11 @@ async def handle_updates(client: Client, channels_config: ChannelsConfig):
     """
     original_chat = await client.get_chat(channels_config.original_channel_id)
 
-    await update_channel_description(client, channels_config)
+    await update_channel_info(client, channels_config)
     await forward_messages(client, original_chat, channels_config)
 
 
-async def update_channel_description(client: Client, channels_config: ChannelsConfig):
+async def update_channel_info(client: Client, channels_config: ChannelsConfig):
     """
     Updates profile
     :param client: A pyrogram client
@@ -33,11 +33,13 @@ async def update_channel_description(client: Client, channels_config: ChannelsCo
     original_chat = await client.get_chat(channels_config.original_channel_id)
     duplicate_chat = await client.get_chat(channels_config.duplicate_channel_id)
 
-    if original_chat.description == duplicate_chat.description:
-        return
+    if original_chat.description != duplicate_chat.description:
+        logging.log(level=logging.INFO, msg="Updating channel's description")
+        await duplicate_chat.set_description(original_chat.description)
 
-    logging.log(level=logging.INFO, msg="Updating channel's description")
-    await duplicate_chat.set_description(original_chat.description)
+    if original_chat.title != duplicate_chat.title:
+        logging.info("Updating channel's title")
+        await duplicate_chat.set_title(original_chat.title)
 
 
 async def forward_messages(client: Client, original_channel_id: Chat, channels_config: ChannelsConfig):
