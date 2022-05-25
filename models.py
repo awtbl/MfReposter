bc9e868275c60c5dcbd2fa75a1ce526735984bb3
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2022-05-25 22:15:55 +0500

    Now, Channel's model inherited from SQLModel

--- models.py
+++ models.py
@@ -1,21 +1,12 @@
-from peewee import Model, IntegerField, SqliteDatabase
-
-db = SqliteDatabase('data.db')
+from sqlmodel import SQLModel, Field
 
 
-class Channel(Model):
+class Channel(SQLModel):
     """
     The class for containing channel's metadata like
         Channel's ID,
         Channel's last post ID
     """
 
-    channel_id = IntegerField()
-    last_post_id = IntegerField()
-
-    class Meta:
-        database = db
-
-        indexes = (
-            (("channel_id",), False),
-        )
+    channel_id: int
+    last_post_id: int = Field(default=0)
